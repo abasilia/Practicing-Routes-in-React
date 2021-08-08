a0863b2d8ecb41f7a3a58a49d60c49db557b93b2
abasilia <68079654+abasilia@users.noreply.github.com>
2021-08-08 19:05:02 -0400

    8-8-2021

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,28 +1,38 @@
-import { useParams, Route } from "react-router-dom";
-import Comments from '../components/comments/Comments';
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
-const DUMMY_QUOTES=[
-  {id: 'q1', author: 'Alex', text: 'Learnings React'},
-  {id: 'q2', author: 'David', text: 'Learnings Vue'},
-  {id: 'q3', author: 'Michael', text: 'Learnings Angular'},
-  {id: 'q4', author: 'Alex', text: 'Learnings D3.js'},
-  {id: 'q5', author: 'David', text: 'Learnings Node.js'},
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Alex", text: "Learnings React" },
+  { id: "q2", author: "David", text: "Learnings Vue" },
+  { id: "q3", author: "Michael", text: "Learnings Angular" },
+  { id: "q4", author: "Alex", text: "Learnings D3.js" },
+  { id: "q5", author: "David", text: "Learnings Node.js" },
 ];
 
 const QuoteDetail = () => {
+
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote=DUMMY_QUOTES.find(quote=>quote.id===params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   if (!quote) {
-    return <p>No Quote Found</p>
+    return <p>No Quote Found</p>;
   }
 
   return (
     <>
       <h1>Quote Detail Page</h1>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`} >
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
